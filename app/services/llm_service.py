a88f9d713571751f635d0b755c40352de3193604
junaid khan <junaid.apj@gmail.com>
2026-05-21 23:28:43 +0530

    added screenshots for evidence

--- app/services/llm_service.py
+++ app/services/llm_service.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 # from langchain_openai import ChatOpenAI # commenting since limit is exceeded for open ai 
 from langchain_groq import ChatGroq
 
-load_dotenv()
+load_dotenv(override=True)
 
 # llm = ChatOpenAI(
 #     model="gpt-4o-mini",
@@ -12,10 +12,11 @@ load_dotenv()
 # )
 
 llm = ChatGroq(
+    groq_api_key=os.getenv("GROQ_API_KEY"),
     model="llama-3.1-8b-instant",
     temperature=0
 )
-
+print(os.getenv("GROQ_API_KEY"))
 
 def generate_review(prompt: str):
 
